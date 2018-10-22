ade83f85bedfaf6e7b6eeef9d17d723d63b4fcf5
sangrok <lsrock1@naver.com>
2018-10-23 01:52:53 +0900

    bug

--- models/ConvFrontend.py
+++ models/ConvFrontend.py
@@ -7,7 +7,7 @@ import torch.nn.init as init
 class ConvFrontend(nn.Module):
     def __init__(self, options):
         super(ConvFrontend, self).__init__()
-        self.conv = nn.Conv3d(3 if options['model']['coord'] else 1, 64, (5,7,7), stride=(1,2,2), padding=(2,3,3))
+        self.conv = nn.Conv3d(4 if options['model']['coord'] else 1, 64, (5,7,7), stride=(1,2,2), padding=(2,3,3))
         self.norm = nn.BatchNorm3d(64)
         self.pool = nn.MaxPool3d((1,3,3), stride=(1,2,2), padding=(0,1,1))
         if options['model']['coord']:
