f4712b27a1ba49fd2f8c116c0b36c9fba691a850
sangrok <lsrock1@naver.com>
2018-10-23 01:55:51 +0900

    bug

--- data/dataset.py
+++ data/dataset.py
@@ -57,6 +57,7 @@ class Video(list):
         return super().__getitem__(key)
 
     def videoRead(self, item):
+        print(item)
         tmp = []
         cap = cv2.VideoCapture(item)
         while(True):
