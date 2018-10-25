2be9c22dc5359d98a35a665600c0c1e5f7068dd3
sangrok <lsrock1@naver.com>
2018-10-25 11:50:21 +0900

    update log

--- main.py
+++ main.py
@@ -69,12 +69,13 @@ def main():
         os.mkdir(os.path.join(options["general"]["save_path"], options['name'], 'models'))
     
     if(options["general"]['model_load']):
-        path = sorted(glob(os.path.join(options["general"]["save_path"], options['name'], 'models', '*.pth')), key=lambda name : int(name.split('/')[-1].replace('.pth', '').replace('model', '')))
+        path = glob(os.path.join(options["general"]["save_path"], options['name'], 'models', 'model{}.pth'.format(args.start - 2)))
+        #path = sorted(glob(os.path.join(options["general"]["save_path"], options['name'], 'models', '*.pth')), key=lambda name : int(name.split('/')[-1].replace('.pth', '').replace('model', '')))
         if path:
             print('load {} model..'.format(path[-1]))
             model.load_state_dict(torch.load(path[-1]))
         #path = sorted(glob(os.path.join(options["general"]["save_path"], options['name'], 'optimizers', '*.pth')), key=lambda name : int(name.split('/')[-1].replace('.pth', '').replace('optimizer', '')))
-        path = glob(os.path.join(options["general"]["save_path"], options['name'], 'optimizers', 'optimizer{}.pth'.format()))
+        path = glob(os.path.join(options["general"]["save_path"], options['name'], 'optimizers', 'optimizer{}.pth'.format(args.start - 2)))
         if path:
             print('load {} optimizer..'.format(path[-1]))
             optimizer.load_state_dict(torch.load(path[-1]))
