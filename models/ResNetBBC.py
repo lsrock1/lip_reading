629dce07fbdef1f724b005c9c3a8d2dd249e6c3c
sangrok <lsrock1@naver.com>
2018-11-04 15:57:40 +0900

    update options

--- models/ResNetBBC.py
+++ models/ResNetBBC.py
@@ -191,15 +191,15 @@ class ResNet(nn.Module):
     def forward(self, x, landmark=None):
         x, attn = self.layer1(x, landmark if self.attn == 'cbam' else False)
         if self.r1:
-            x = self.r1(x, landmark)
+            x, landmark = self.r1(x, landmark)
 
         x, attn = self.layer2(x, attn if self.attn == 'cbam' else False)
         if self.r2:
-            x = self.r2(x, landmark)
+            x, landmark = self.r2(x, landmark)
 
         x, _ = self.layer3(x, attn if self.attn == 'cbam' else False)
         if self.r3:
-            x = self.r3(x, landmark)
+            x, _ = self.r3(x, landmark)
         del _
         x, _ = self.layer4(x)
 
